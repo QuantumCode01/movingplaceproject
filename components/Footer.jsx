e8c144fbd4cde55444b7ca5d4168e2e0f1f37b2c
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-05 03:50:25 +0530

    Update Footer.jsx
    changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-white border-t border-gray-200 py-6 text-gray-600 text-sm font-mulish my-[64px]">
+    <footer className="bg-white border-t border-gray-200  text-gray-600 text-sm font-mulish my-[64px] sm:pt-[64px] pt-[48px]">
       <div className="max-w-7xl mx-auto px-10 sm:px-6 lg:px-8">
-    
-        <div className="flex flex-col md:flex-row  justify-between gap-4 mb-6">
+ 
+        <div className="flex flex-col md:flex-row  justify-between gap-[32px]   mb-[64px]">
           <div className="flex items-center gap-2">
             <img src="../assets/Logo.png" alt="Logo" className="w-[195px] h-[35px]" />
           </div>
@@ -17,51 +17,55 @@ export default function Footer() {
           </a>
         </div>
 
-      
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6 text-[#2E343E] font-medium">
+     
+        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6  text-[#2E343E] font-normal text-[16px]">
           <div>
             Contact us:{' '}
-            <a href="tel:8666059518" className="font-semibold">
+            <a href="tel:8666059518" className="font-normal">
               866-605-9518
             </a>
           </div>
 
-        
-          <div className="flex flex-col sm:flex-row justify-center sm:justify-start gap-2 sm:gap-4  sm:text-left">
-            <a href="#" className="hover:underline">
+          {/* Stack vertically on mobile */}
+          <div className="flex flex-col sm:flex-row justify-center sm:justify-start gap-2 sm:gap-4  sm:text-left text-[#5B5E5F] text-[16px]">
+            <a href="#" className="hover:underline font-bold font-mulish">
               Customer Log in
             </a>
-            <a href="#" className="hover:underline">
+            <a href="#" className="hover:underline font-bold font-mulish">
               Provider Log in
             </a>
-            <a href="#" className="hover:underline">
+            <a href="#" className="hover:underline font-bold font-mulish">
               License & Insurance
             </a>
-            <a href="#" className="hover:underline">
+            <a href="#" className="hover:underline font-bold font-mulish">
               Cancellation Policy
             </a>
-            <a href="#" className="hover:underline">
+            <a href="#" className="hover:underline font-bold font-mulish">
               Service Guarantee
             </a>
-            <a href="#" className="hover:underline">
+            <a href="#" className="hover:underline font-bold font-mulish">
               Do not sell my info
             </a>
           </div>
         </div>
 
+     
         <hr className="border-gray-200 mb-4" />
 
-      
-        <div className="flex flex-col sm:flex-row sm:justify-between text-xs text-[#2E343E] gap-2">
-          <p>Copyright 2024 MovingPlace LLC. All Rights Reserved.</p>
+   
+        <div className="flex flex-col sm:flex-row sm:justify-between text-xs text-[#84878A] gap-2 h-[29px]">
+          <p className='mr-auto'>Copyright 2024 MovingPlace LLC. All Rights Reserved.</p>
           <p>License #MC807665. 2200 1st Ave S, Seattle, WA 98134</p>
           <div className="flex gap-4 text-blue-600">
+             <span className='text-[#CFD1D2] w-px h-5  inline-block'>|</span>
             <a href="#" className="hover:underline">
               Terms
             </a>
+             <span className='text-[#CFD1D2]'>|</span>
             <a href="#" className="hover:underline">
               Privacy
             </a>
+             <span className='text-[#CFD1D2]'>|</span>
             <a href="#" className="hover:underline">
               Sitemap
             </a>
