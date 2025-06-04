8967d6b8af469ff84172a8709d7a51b08a9b5f1d
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-05 03:49:33 +0530

    Update Card.jsx
    changes

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,19 +6,19 @@ export default function Card({  image="", title="", subtitle="", description="",
     <div className=" rounded-[24px]  shadow-md p-[32px] w-full max-w-md "  style={{ backgroundColor: bgColor }}>
       <img src={image} alt={title} className="rounded-xl mb-4 w-full h-44 object-cover" />
   
-      <h3 className="text-2xl font-semibold mb-1 " style={{ color: titleColor }}>{title}</h3>
+      <h3 className="lg:text-[30px] text-[24px] font-semibold mb-1 " style={{ color: titleColor }}>{title}</h3>
 
-      <p className="font-semibold text-sm mb-3 flex items-center gap-2"  style={{ color: subtitleColor }}>
+      <p className="font-semibold text-[18px] md:mb-[24px] mb-[16px] flex items-start gap-2"  style={{ color: subtitleColor }}>
         {iconImage && (
-          <img src={iconImage} alt="icon" className="w-[12px] h-[12px]" />
+          <img src={iconImage} alt="icon" className="w-[12px] h-[12px] mt-[8px]" />
         )}
         {subtitle}
       </p>
 
-      <p className=" mb-4" style={{ color: descriptionColor }}>{description}</p>
+      <p className=" md:mb-[24px] mb-[16px]" style={{ color: descriptionColor }}>{description}</p>
       
       {buttonText && (
-      <button className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-semibold px-4 py-2 rounded-lg shadow-md">
+      <button className="bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-semibold px-4 py-2 rounded-lg shadow-md w-full sm:w-[227px]">
           {buttonText}   
       </button>
         )} 
