3ad84215e2bafc27183d979e70d77e3c9b0b79e4
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-05 03:48:33 +0530

    Update Advantage.jsx
    changes

--- components/Advantage.jsx
+++ components/Advantage.jsx
@@ -3,23 +3,23 @@ import Image from "next/image";
 export default function Advantage() {
   return (
     <div>
-           <div className="hero  min-h-screen">
-                <div className="hero-content flex-col lg:flex-row-reverse gap-16 items-start">
-                    <div className=" w-full sm:w-[55%] px-4 py-5 max-w-3xl mx-auto">
+           <div className="hero  min-h-screen sm:py-[64px] py-[32px] lg:px-[64px] px-[16px] md:px-[32px]">
+                <div className="hero-content flex-col lg:flex-row-reverse gap-[64px] items-start px-0">
+                    <div className=" w-full lg:w-[55%]  max-w-3xl mx-auto order-2 lg:order-1 ">
 
-                        <h2 className="text-4xl font-extrabold text-gray-800 mb-6 leading-tight">
+                        <h2 className="text-[#2E343E] text-[36px] sm:text-[48px] font-extrabold  mb-[40px] leading-tight ">
                             The MovingPlace <br />
                             Advantage
                         </h2>
 
 
-                        <p className="text-gray-600 text-base mb-6">
+                        <p className="text-[#2E343E] text-[16px] mb-6">
                             At MovingPlace, we’ve reimagined the way people move—combining technology,
                             trusted professionals, and a customer-first approach to deliver a seamless
                             experience from start to finish.
                         </p>
 
-                        <div className="space-y-5 text-gray-600">
+                        <div className="space-y-5 text-[#2E343E]">
                             <div className="flex items-start gap-3">
                             <img src="../assets/arrow-right-circle-fill.svg" alt="arrow-icon" className="w-[24px] h-[24px]"/>
                             <p>
@@ -28,7 +28,7 @@ export default function Advantage() {
                                 <strong>Best Price Guarantee</strong>.
                             </p>
                             </div>
-                            <div className="flex items-start gap-3">
+                            <div className="flex items-start gap-3 text-[#2E343E]">
                             <img src="../assets/arrow-right-circle-fill.svg" alt="arrow-icon" className="w-[24px] h-[24px]"/>
                             <p>
                                 We partner exclusively with
@@ -37,7 +37,7 @@ export default function Advantage() {
                                 your belongings with expertise and care.
                             </p>
                             </div>
-                            <div className="flex items-start gap-3">
+                            <div className="flex items-start gap-3 text-[#2E343E]">
                            <img src="../assets/arrow-right-circle-fill.svg" alt="arrow-icon" className="w-[24px] h-[24px]"/>
                             <p>
                                 Every move is covered by our
@@ -48,21 +48,21 @@ export default function Advantage() {
                         </div>
 
 
-                        <p className="mt-6 text-gray-600">
+                        <p className="mt-6 text-[#2E343E]">
                             No matter what you need, we have you covered. Labor, Packing, Trucks,
                             Long-Distance. We have it all.
                         </p>
 
 
-                        <div className="mt-8">
-                            <button className="flex items-center gap-2 bg-yellow-400 hover:bg-yellow-500 text-gray-900 font-semibold px-6 py-3 rounded-full shadow-md transition">
+                        <div className="mt-4">
+                            <button className="flex items-center gap-2 bg-yellow-400 hover:bg-yellow-500 text-[#2E343E] font-semibold px-6 py-3 rounded-full shadow-md transition">
                             Get a Moving Quote
                            <img src="../assets/blackarrow.svg" alt="arrow" className='w-[24px] h-[24px]' />
                             </button>
                         </div>
 
 
-                        <div className="mt-4">
+                        <div className="mt-[32px]">
                             <a
                             href="#"
                             className="text-blue-600 hover:underline font-semibold text-sm flex items-center gap-1"
@@ -73,8 +73,8 @@ export default function Advantage() {
                         </div>
                     </div>
 
-                    <div className='w-full sm:w-[45%] order-2 sm:order-1 flex items-start'>
-                         <img src="/assets/Advantageimage.png" alt="Stock photo" className="w-[90%] h-auto object-cover rounded-lg"  /> 
+                    <div className='w-full lg:w-[45%] order-1  flex lg:items-start justify-end'>
+                         <img src="/assets/Advantageimage.png" alt="Stock photo" className="lg:w-[90%]  sm:w-[60%] w-full h-auto object-cover rounded-lg"  /> 
                     </div>
                                 
                     
