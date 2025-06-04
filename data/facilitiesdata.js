def53fcf68e4732f4b040ff201ce187ef583550a
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-05 03:55:09 +0530

    Update facilitiesdata.js
    changes

--- data/facilitiesdata.js
+++ data/facilitiesdata.js
@@ -2,26 +2,31 @@ const facilitiesdata = [
   {
     title: "Licensed & Insured Providers",  
     image: "../assets/licensed.png",
+     titleSize:"sm:text-[18px] text-[14px]",
 
   },
   {
     title: "Movers Background-Checked and Tested",
     image: "../assets/movers.png",
+     titleSize:"sm:text-[18px] text-[14px]",
 
   },
   {
     title: "Support Available 7 Days a Week ",
     image: "../assets/support.png",
+     titleSize:"sm:text-[18px] text-[14px]",
 
   },
   {
     title: "Heavy Items, Pianos and Safes Included",
     image: "../assets/heavyitems.png",
+     titleSize:"sm:text-[18px] text-[14px]",
 
   },
   {
     title: "Hand Tools, Dollies & Straps Included",
     image: "../assets/tools.png",
+     titleSize:"sm:text-[18px] text-[14px]",
 
   },
   
