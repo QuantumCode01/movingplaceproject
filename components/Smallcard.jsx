26ccda88bf4dea893c485937c66e1b8ec0138892
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-05 03:52:21 +0530

    Update Smallcard.jsx
    changes

--- components/Smallcard.jsx
+++ components/Smallcard.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 
-export default function Smallcard({title="",description="",image=""}) {
+export default function Smallcard({title="",description="",image="",titleSize=""}) {
   return (
-    <div className="card bg-base-100 px-[16] py-[24]">
+    <div className="card bg-base-100 px-[0] py-[24] flex gap-[32px]">
         <figure className=" ">
             <img
             src={image}
             alt="icon-image"
             className="rounded-xl w-[80] h-[80]" />
         </figure>
-        <div className="card-body items-center text-center px-0 ">
-            <h6 className="card-title">{title}</h6>
-            <p>{description}</p>
+        <div className="card-body items-center text-center px-0 py-0 gap-[8px] flex flex-col">
+            <h6 className={`card-title text-[#2E343E] ${titleSize}`}>{title}</h6>
+            <p className='text-[#2E343E] text-[16px]'>{description}</p>
            
         </div>
     </div>
