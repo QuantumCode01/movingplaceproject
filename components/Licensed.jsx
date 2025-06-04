2a8c7877a3941121c590383090102524d90fc212
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-05 03:51:37 +0530

    Update Licensed.jsx
    changes

--- components/Licensed.jsx
+++ components/Licensed.jsx
@@ -3,21 +3,21 @@ import Image from "next/image";
 export default function Licensed() {
   return (
     <div>
-           <div className="hero bg-base-200 min-h-screen">
+           <div className="hero  min-h-screen">
                 <div className="hero-content flex-col lg:flex-row gap-16">
-                    <div className=" w-full sm:w-[55%] px-4 py-10 max-w-3xl mx-auto">
+                    <div className=" w-full lg:w-[55%] max-w-3xl mx-auto order-2 lg:order-1">
 
-                        <h2 className="text-4xl font-extrabold text-gray-800 mb-6 leading-tight">
+                        <h2 className="text-[#2E343E] text-[36px] sm:text-[48px] font-extrabold  mb-[40px] leading-tight">
                            Licensed and Vetted  <br />
                           Moving Partners
                         </h2>
 
 
-                        <p className="text-gray-600 text-base mb-6">
+                        <p className="text-[#2E343E] text-[16px] mb-6">
                             MovingPlace works exclusively with licensed and insured movers who meet our high standards for quality and reliability, each undergoing a full background check. You can move with confidence, knowing your belongings are in trusted hands.
                         </p>
 
-                        <div className="space-y-5 text-gray-600">
+                        <div className="space-y-5 text-[#2E343E]">
                             <div className="flex items-start gap-3">
                             <img src="../assets/arrow-right-circle-fill.svg" alt="arrow-icon" className="w-[24px] h-[24px]"/>
                             <div>
@@ -32,7 +32,7 @@ export default function Licensed() {
                             <img src="../assets/arrow-right-circle-fill.svg" alt="arrow-icon" className="w-[24px] h-[24px]"/>
                             <div>
                                 <strong className='text-[#E0B138]'>Checked and Tested!</strong>
-                               <p>
+                               <p className='text-[#2E343E]'>
                                     Each provider is thoroughly background-checked and tested for quality, ensuring you receive reliable service from trusted professionals.
                                </p>
                             </div>
@@ -42,12 +42,8 @@ export default function Licensed() {
                         </div>
 
 
-                        <p className="mt-6 text-gray-600">
-                            No matter what you need, we have you covered. Labor, Packing, Trucks,
-                            Long-Distance. We have it all.
-                        </p>
-
-                        <div className="mt-4">
+                       
+                        <div className="mt-[32px] ml-[20px]">
                             <a
                             href="#"
                             className="text-blue-600 hover:underline font-semibold text-sm flex items-center gap-1"
@@ -58,7 +54,7 @@ export default function Licensed() {
                         </div>
                         </div>
 
-                    <div className='w-full sm:w-[45%] order-2 sm:order-1'>
+                    <div className='w-full lg:w-[45%] order-1 sm:order-1'>
                          <img src="/assets/licensedimage.png" alt="Stock photo" className="w-full h-auto object-cover rounded-lg"  /> 
                     </div>
                                 
