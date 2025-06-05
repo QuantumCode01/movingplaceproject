95742afc007142c608d6c449cec6f279e3e92e5b
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-06 03:43:42 +0530

    Add files via upload
    changes

---
+++ data/testimonialdata.js
@@ -0,0 +1,26 @@
+// components/data/testimonialData.js
+const testimonials = [
+  {
+    stars: 5,
+    name: "Sandra D",
+    city: "San Francisco",
+    date: "05/15/30",
+    text: `Booking with MovingPlace made finding movers so simple and stress-free! `,
+  },
+  {
+    stars: 5,
+    name: "John S",
+    city: "Miami",
+    date: "04/20/30",
+    text: `Very easy to book and communicate. Everything went smoothly.`,
+  },
+  {
+    stars: 4,
+    name: "Emily R",
+    city: "Chicago",
+    date: "03/10/30",
+    text: `Great service. Slight delay but customer support was helpful.`,
+  },
+];
+
+export default testimonials;
