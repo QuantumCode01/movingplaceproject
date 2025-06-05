8fa769946b9910a269bd7e857b0a09547448522d
QuantumCode01 <121271945+QuantumCode01@users.noreply.github.com>
2025-06-06 03:39:22 +0530

    Update Footer.jsx
    changes

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <footer className="bg-white border-t border-gray-200  text-gray-600 text-sm font-mulish my-[64px] sm:pt-[64px] pt-[48px]">
+    <footer className="bg-white  max-w-[1400px] mx-auto border-t border-gray-200  text-gray-600 text-sm font-mulish my-[64px] sm:pt-[64px] pt-[48px]">
       <div className="max-w-7xl mx-auto px-10 sm:px-6 lg:px-8">
  
         <div className="flex flex-col md:flex-row  justify-between gap-[32px]   mb-[64px]">
@@ -18,7 +18,7 @@ export default function Footer() {
         </div>
 
      
-        <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 mb-6  text-[#2E343E] font-normal text-[16px]">
+        <div className="flex flex-col lg:flex-row lg:items-center lg:justify-between  items-start gap-[32px] mb-6  text-[#2E343E] font-normal text-[16px]">
           <div>
             Contact us:{' '}
             <a href="tel:8666059518" className="font-normal">
@@ -27,7 +27,7 @@ export default function Footer() {
           </div>
 
           {/* Stack vertically on mobile */}
-          <div className="flex flex-col sm:flex-row justify-center sm:justify-start gap-2 sm:gap-4  sm:text-left text-[#5B5E5F] text-[16px]">
+          <div className="flex flex-col md:flex-row md:flex-wrap justify-center sm:justify-start gap-[32px] sm:gap-4  sm:text-left text-[#5B5E5F] text-[16px]">
             <a href="#" className="hover:underline font-bold font-mulish">
               Customer Log in
             </a>
@@ -53,10 +53,10 @@ export default function Footer() {
         <hr className="border-gray-200 mb-4" />
 
    
-        <div className="flex flex-col sm:flex-row sm:justify-between text-xs text-[#84878A] gap-2 h-[29px]">
+        <div className="flex flex-col lg:flex-row sm:justify-between text-[14px] text-[#84878A] gap-[16px] h-[29px]">
           <p className='mr-auto'>Copyright 2024 MovingPlace LLC. All Rights Reserved.</p>
           <p>License #MC807665. 2200 1st Ave S, Seattle, WA 98134</p>
-          <div className="flex gap-4 text-blue-600">
+          <div className="flex gap-4 text-blue-600 lg:mt-[0px] mt-[10px]">
              <span className='text-[#CFD1D2] w-px h-5  inline-block'>|</span>
             <a href="#" className="hover:underline">
               Terms
